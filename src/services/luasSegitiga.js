cd62c71dca764aecacc6b0814231080759198c36
nurliman <nurlimandiara@gmail.com>
2022-01-20 09:31:56 +0700

    .

--- src/services/luasSegitiga.js
+++ src/services/luasSegitiga.js
@@ -1,7 +1,7 @@
 module.exports = function menghitungLuasSegitiga(req, res) {
   const alas = req.query.alas;
   const tinggi = req.query.tinggi;
-  const luas = (alas * tinggi) / 2;
+  const luas = (alas * tinggi) / 2 + 1;
 
   res.json({ luas });
 };
